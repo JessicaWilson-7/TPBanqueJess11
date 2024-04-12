d41c84cb5ff4239f7996ed0653e014fd37c57139
Jess <nomenjanaharimananajessica@gmail.com>
2024-04-12 12:44:38 +0300

    TP4: Config,template,page ListeCompte,et test

---
+++ src/main/java/com/company/tpbanquejess11/jsf/ListeComptes.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.company.tpbanquejess11.jsf;
+
+import com.company.tpbanquejess11.entity.CompteBancaire;
+import com.company.tpbanquejess11.service.GestionnaireCompte;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * Author: USER
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    public List<CompteBancaire> getAllComptes() {
+        return gestionnaireCompte.getAllComptes();
+    }
+}
